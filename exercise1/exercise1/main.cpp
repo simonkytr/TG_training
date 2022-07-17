80be7f1586ec82c163ca747d3d5af99519b3efbc
simonkytr <109240067+simonkytr@users.noreply.github.com>
2022-07-17 16:19:48 -0500

    I've alredy corrected the text

--- exercise1/exercise1/main.cpp
+++ exercise1/exercise1/main.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 //Exercise 1 - Hello World
-//Write a program that will output “Hello World!” in the first line, then your nameand something about yourself in the second line
+//Write a program that will output â€œHello World!â€ in the first line, then your nameand something about yourself in the second line
 
 
 int main()
 {
-	std::cout << "hello word¡" << std::endl;
+	std::cout << "Hello World!" << std::endl;
 	std::cout << "my name is Diego and I'm so thrilled to start this internship" << std::endl;
 	std::cin.get();
 
