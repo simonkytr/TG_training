c2a2ad41814dc2f1a7ab32ba7f6ad68fb2ae6e43
simonkytr <109240067+simonkytr@users.noreply.github.com>
2022-08-11 08:30:19 -0500

    SteamReview

---
+++ SteamReview/SteamReview/Category.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include "Category.h"
+
+FCategory::FCategory()
+{
+	CategoryName = "";
+	GameCount = 0;
+};
+
+//-------------------------------------------------
+FCategory::FCategory(std::string InCategoryName)
+{
+	GameCount = 0;
+	CategoryName = InCategoryName;
+};
+
+//-------------------------------------------------
+std::string FCategory::GetCategoryName() const
+{
+	return CategoryName;
+};
+//-------------------------------------------------
+bool FCategory::IsCategoryFull() const
+{
+	return GameCount == MaxGamesCount;
+};
+
+//-------------------------------------------------
+bool FCategory::AddGame(const FGame InGame)
+{
+	if (!IsCategoryFull() && IsCategoryValid())
+	{
+		Games[GameCount] = InGame;
+		GameCount++;
+		return true;
+	}
+	return false;
+};
+
+//-------------------------------------------------
+FGame FCategory::GetGame(const int GameIndex) const
+{
+	return Games[GameIndex];
+};
+
+//-------------------------------------------------
+bool FCategory::IsCategoryValid() const
+{
+	return CategoryName != "";
+};
+
+//-------------------------------------------------
+void FCategory::DisplayCategory() const
+{
+	std::cout << "\nCategory: " << CategoryName << std::endl;
+	std::cout << "\nGame Name" << "\tDeveloper Studio" << "\tRelease Date" << std::endl;
+	std::cout << "-----------------------------------------------------------------" << std::endl;
+	for (int i = 0; i < MaxGamesCount; i++)
+	{
+		std::cout << Games[i].GetGameName() << "\t" << Games[i].GetGameStudio() << "\t" << Games[i].GetGameDateFormat() << std::endl;
+	}
+};
---
+++ SteamReview/SteamReview/Game.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <iostream>
+class FGame
+{
+private:
+	std::string NameGame;
+	std::string StudioGame;
+	int Day;
+	int Month;
+	int Year;
+public:
+	FGame();
+	FGame(std::string InName, std::string InStudio, int InDay, int InMonth, int InYear);
+	std::string GetGameName() const;
+	std::string GetGameStudio() const;
+	std::string GetGameDateFormat() const;
+	bool IsValid() const;
+};
