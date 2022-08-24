a513c77362b773bfed9069485bedd704275b51d4
simonkytr <109240067+simonkytr@users.noreply.github.com>
2022-08-24 09:21:18 -0500

    Update Source.cpp

--- Pointers_Excercises/Excercise1/Source.cpp
+++ Pointers_Excercises/Excercise1/Source.cpp
@@ -1,3 +1,12 @@
+/*Exercises
+Exercise 1 - Understanding Memory Addresses
+Create a program that will ask the user to enter an integer and assign the given value to a
+variable. Then, print the value to the console, and print the memory address of that variable.
+Then, ask for another value and assign that value to the same variable. Print the value and the
+memory address to the console.
+Finally, ask for one more value. This time, assign the value to a different variable. Print the
+value and the memory address to the console. Do the results make sense to you?*/
+
 #include<iostream>
 
 int main()
