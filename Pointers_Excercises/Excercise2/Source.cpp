c6be63567f882672fb54ed11a86a29754ca573ec
simonkytr <109240067+simonkytr@users.noreply.github.com>
2022-08-24 09:21:53 -0500

    Update Source.cpp

--- Pointers_Excercises/Excercise2/Source.cpp
+++ Pointers_Excercises/Excercise2/Source.cpp
@@ -1,3 +1,11 @@
+/*Exercise 2 - Understanding Pointers
+Create a program that will ask the user for an integer value. Assign the value to an integer
+variable.
+Then, create an int* variable and assign it to the memory address of the integer variable.
+Ask the user to enter a new value for the variable. Then, using only the pointer variable, change
+the value of the integer variable to the new value.
+Print the resulting value of variable a.*/
+
 #include <iostream>
 
 int main()
@@ -13,5 +21,5 @@ int main()
 	std::cin >> *InputValuePtr;
 	std::cout << "The value of variable a is now: " << InputValue << std::endl;
 
-	std::cout << "\nFun fact: I didn’t have to touch a directly to change its value :)" << std::endl;
+	std::cout << "\nFun fact: I didnâ€™t have to touch a directly to change its value :)" << std::endl;
 }
