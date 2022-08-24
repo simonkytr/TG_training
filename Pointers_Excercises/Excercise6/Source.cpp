95e5d848ee9b05137c52ad1f4f86a51d72c401bb
simonkytr <109240067+simonkytr@users.noreply.github.com>
2022-08-24 09:24:54 -0500

    Update Source.cpp

--- Pointers_Excercises/Excercise6/Source.cpp
+++ Pointers_Excercises/Excercise6/Source.cpp
@@ -1,3 +1,15 @@
+/*Exercise 6 - Pointer Arithmetic
+Pointer arithmetic allows you to perform arithmetic operations on pointer types.
+Create a program where you declare an array of 10 integers. Then, declare an int pointer and
+initialize it to the array.
+
+Loop through all of the elements in the array and initialize each entry in the array with a random
+integer. You must use ONLY the pointer to loop through the array. Print each number to the
+console.
+Then, loop through the array BACKWARDS to get the sum of all the elements. Print the sum to
+the console.
+*/
+
 #include <iostream>
 #include <stdio.h>      /* printf, scanf, puts, NULL */
 #include <stdlib.h>     /* srand, rand */ 
