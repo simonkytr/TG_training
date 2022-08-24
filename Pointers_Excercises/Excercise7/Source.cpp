92c632e975c2cb796861de978dcea0329100f84d
simonkytr <109240067+simonkytr@users.noreply.github.com>
2022-08-24 09:25:23 -0500

    Update Source.cpp

--- Pointers_Excercises/Excercise7/Source.cpp
+++ Pointers_Excercises/Excercise7/Source.cpp
@@ -1,3 +1,17 @@
+/*Exercise 7 - Pointer validity
+Declare the following function:
+void incrementNumber(int* numberPtr);
+The function should increase the value of whatever integer the pointer is pointing to.
+In the main function, create an integer and assign it a value. Create an int pointer and initialize it
+to be a null pointer. Then, assign the memory address of your integer to it. Pass in your pointer
+to the incrementNumber function and print the result to the screen. Run your program.
+After making sure the function is working fine, comment out the line where you are initializing
+the pointer to the memory address of your int.
+Depending on whether you validated the pointer or not, your program may have crashed. This is
+one of the dangers of pointers.
+Accessing null pointers will crash your application, so it’s important to be sure to add validity
+checks when there is even the slightest possibility that the value of a pointer will be null!*/
+
 #include <iostream>
 
 void IncrementNumber(int* NumberPtr)
