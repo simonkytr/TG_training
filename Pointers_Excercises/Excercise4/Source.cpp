e7d0f6a5b14e7970455fbd32d08543623064645b
simonkytr <109240067+simonkytr@users.noreply.github.com>
2022-08-24 09:23:19 -0500

    Update Source.cpp

--- Pointers_Excercises/Excercise4/Source.cpp
+++ Pointers_Excercises/Excercise4/Source.cpp
@@ -1,3 +1,10 @@
+/*Exercise 4 - Understanding Pointers 3
+Create a program where you declare an int variable and initialize it to whatever value you want.
+Print the value to the console.
+Then, create two int pointer variables that point to the variable you just created.
+Dereference the first pointer and add one to the dereferenced value. Do the same thing with the
+second pointer. Print the final value of your initial variable. Do these results make sense to you?*/
+
 #include <iostream>
 
 int main()
