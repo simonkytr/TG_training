465b47283af6e3169f661c6729204e95d3321561
simonkytr <109240067+simonkytr@users.noreply.github.com>
2022-09-07 08:39:50 -0500

    Exercise5: StaticArrat access

--- Array_Vector_Exercises/Exercise1/TStaticArray.h
+++ Array_Vector_Exercises/Exercise1/TStaticArray.h
@@ -1,9 +1,54 @@
 #pragma once
 
-template <typename T, int N>
+template <typename T, const int N>
 class TStaticArray
 {
-	static SizeArray = N;
-
+	static const SizeArray = N;
 	T StaticArray[SizeArray];
+
+	//---------------------------------------------------------
+	T operator[int Index]
+	{
+		return StaticArray[Index];
+	};
+
+	T operator[int Index] const
+	{
+		return StaticArray[Index];
+	};
+
+	//---------------------------------------------------------
+	T Front()
+	{
+		return StaticArray[0];
+	};
+
+	T Front() const
+	{
+		return StaticArray[0];
+	};
+
+	//---------------------------------------------------------
+	T Back()
+	{
+		return StaticArray[SizeArray - 1];
+	};
+
+	T Back() const
+	{
+		return StaticArray[SizeArray - 1];
+	};
+
+	//---------------------------------------------------------
+	T GetData()
+	{
+		T* StaticArrayPtr = &StaticArray;
+		return StaticArrayPtr;
+	};
+
+	T GetData() const
+	{
+		T* StaticArrayPtr = &StaticArray;
+		return StaticArrayPtr;
+	};
 };
