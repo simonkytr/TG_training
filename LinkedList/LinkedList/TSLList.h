1a3e7b131bec5c1ae51f0780f2b995769ec170c1
simonkytr <109240067+simonkytr@users.noreply.github.com>
2022-09-13 14:42:27 -0500

    exercise 4: modifiers

--- LinkedList/LinkedList/TSLList.h
+++ LinkedList/LinkedList/TSLList.h
@@ -70,5 +70,90 @@ public:
 		return Size;
 	}
 
+	//-----------------------------------Modifiers
+	bool AddHead(const Type& InHead)
+	{
+		FNode* NewHead = new FNode;
+		NewHead->Element = InHead;
+
+		if (Head == nullptr)
+		{
+			Head = NewHead;
+			return true;
+		}
+		else
+		{
+			NewHead->Next = Head;
+			Head = NewHead;
+			return true;
+		}
+		return false;
+	}
+
+	//-------------------------------------------------
+	bool AddTail(const Type& InTail)
+	{
+		FNode* NewTail = new FNode;
+		NewTail->Element = InTail;
+
+		if (Tail == nullptr)
+		{
+			Tail = NewTail;
+			return true;
+		}
+		else
+		{
+			NewTail->Next = Tail;
+			Tail = NewTail;
+			return true;
+		}
+		return false;
+	}
 	
+	//----------------------------------------------------
+	void Insert(const int Index, const Type& NewItem)
+	{
+		FNode* Temporal = Head;
+		FNode* Container = new FNode;
+
+		for (int i = 0; i < index; i++)
+		{
+			Temporal = Temporal->Next;
+		}
+		Container->Element = Temporal->Element;
+		Temporal->Element = NewItem;
+		Temporal->Next = Container;
+		Size++;
+	}
+
+	//---------------------------------------------------------
+	void Remove(const int Index)
+	{
+		FNode* Temporal = Head;
+
+		for (int i = 0; i < index - 1; i++)
+		{
+			Temporal = Temporal->Next;
+		}
+
+		FNode* Current = Temporal->Next;
+		Temporal->Next = Current->Next;
+		delete Current;
+		
+	}
+
+	//----------------------------------------------------------
+	void Clear()
+	{
+		FNode* NextItem = new FNode;
+
+		for (int i = 0; i < Size; i++)
+		{
+			NextItem = Head->Next;
+			delete Head;
+			Head = NextItem;
+		}
+	}
+
+	//---------------------------------------------------------
 };
